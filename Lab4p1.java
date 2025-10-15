2cf2e6856ec5c2a9527973cc8dd5cd414a9a27c4
santhosh3898 <santhoshchanna6@gmail.com>
2025-10-15 10:31:35 +0530

    Add files via upload

---
+++ Lab4p1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x,y;
+        Scanner s=new Scanner(System.in);
+        try
+        {
+        x= s.nextInt();
+        y= s.nextInt();
+        System.out.println(x/y);
+    }
+    catch(InputMismatchException ie){
+          System.out.println(ie.getClass().getName());
+        
+    }
+    catch(ArithmeticException ie) {
+    System.out.println(ie.getClass().getName() + ": / by zero");}
+    }
+} 
+    
