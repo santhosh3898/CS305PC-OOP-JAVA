2211cc0874f10b1357ad1ca38d82a87019969423
santhosh3898 <santhoshchanna6@gmail.com>
2025-10-30 17:19:06 +0530

    Add Lab3P51 class with demonstration methods

---
+++ LAB3/Lab3p51.java
@@ -0,0 +1,13 @@
+class Lab3P51{
+	String name;
+	public void demonstrate(){
+		System.out.println(" I am a method in Lab3P51");
+	} 
+	public void getName(String s){
+		this.name = s;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+}
