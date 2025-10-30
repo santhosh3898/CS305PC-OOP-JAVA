553a4cce1ae6fc2e6b6fc843991556a817b742c6
santhosh3898 <santhoshchanna6@gmail.com>
2025-10-30 17:00:51 +0530

    Add Lab1P3.java for basic division operation

---
+++ LAB1/Lab1P3.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class Lab1P3{
+	public static void main(String...args){
+		System.out.println("enter the numbers");
+		Scanner s = new Scanner(System.in);
+			int a = s.nextInt();
+			int b = s.nextInt();
+			
+			var c = a/b;
+			System.out.println(c); 
+	}
+	
+}
