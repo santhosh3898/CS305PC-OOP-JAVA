b3ad7cee3514d3c1be467b196980377395904f41
santhosh3898 <santhoshchanna6@gmail.com>
2025-10-30 17:21:30 +0530

    Add Lab3P5M class with main method

---
+++ LAB3/Lab3p5m.java
@@ -0,0 +1,9 @@
+class Lab3P5M{
+	public static void main (String...args){
+		Lab3P52 p = new Lab3P52();
+		
+		p.setName("ACE ENGINEERING COLLEGE");
+		System.out.println(p.getName());
+		p.demonstrate();
+	}
+}
