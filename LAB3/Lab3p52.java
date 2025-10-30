9688efc9ebda90d82b080f9b2bc9461f7838e8a1
santhosh3898 <santhoshchanna6@gmail.com>
2025-10-30 17:20:03 +0530

    Add Lab3P52 class extending Lab3P51

---
+++ LAB3/Lab3p52.java
@@ -0,0 +1,12 @@
+
+class Lab3P52 extends Lab3P51{
+	String name;
+	
+	@Override
+	public void demonstrate(){
+		System.out.println(" I am a method in Lab3P51");
+	} 
+	Lab3P52(){
+		super.demonstrate();
+	}
+}
